becdcd169ff8d99f948523529a8677c612097da0
Cecilia Swahn Gleisner <cecilia@lifeistooshort.se>
2024-03-03 15:20:25 +0000

    The rest operator syntax

--- rest-parameter-syntax.js
+++ rest-parameter-syntax.js
@@ -3,8 +3,30 @@
  * command node rest-parameter-syntax.js in the terminal
  */
 
+/* Allows us to handle the contents of arrays of multiple lengths. By using the ... 
+*three dots rest parameter */
+
 // Regular function call 
+
+const sumAll = (a, b, c) => a + b + c;
+let sum = sumAll(1, 2, 3);
+console.log(sum);
 
 // Extra arguments are ignored
 
+let sum2 = sumAll(1, 2, 3, 4, 5, 6);
+console.log("Sum2:", sum2);
+
 // Function using ...rest
+
+sumRest = (a, b, c, ...rest) => {
+    let sum = a + b + c;
+    for (let i of rest) {
+        sum += i;
+    }
+    return sum;    
+}
+
+let sum3 = sumRest(1, 2, 3, 4, 5, 6);
+
+console.log("Sum3:", sum3);
